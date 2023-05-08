81fcced933e346293806d2f7b9bdb44b91c1216a
mufeng <127177968+mufengyaa@users.noreply.github.com>
2023-05-08 23:27:50 +0800

    Date类 - 更新

--- date.cpp
+++ date.cpp
@@ -2,7 +2,7 @@
 
 #include"date.h"
 
-int Date::GetMonthDay(int year, int month) {
+int Date::GetMonthDay(int year, int month) const{
 	static int m[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
 	if (month == 2) {
 		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
@@ -17,70 +17,96 @@ int Date::GetYearDay(int year) {
 	}
 	return 365;
 }
-Date::Date(int year, int month, int day) {		
-	this->_year = year;
-	this->_month = month;
-	this->_day = day;
+Date::Date(int year, int month, int day){		
+	if (month > 0 && month < 13 && day > 0 && day <= (*this).GetMonthDay(_year, month)) {
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	else {
+		cout << "输入非法日期" << endl;
+		assert(false);
+	}
 }
-//Date::~Date() {
-//	this->_year = 0;
-//	this->_month = 0;
-//	this->_day = 0;
-//}
+Date::~Date() {
+	_year = 0;
+	_month = 0;
+	_day = 0;
+}
 Date::Date(const Date& d) {
-	this->_year = d._year;
-	this->_month = d._month;
-	this->_day = d._day;
+	_year = d._year;
+	_month = d._month;
+	_day = d._day;
 }
 Date& Date::operator=(const Date& d) {
 	if (this != &d) {
-		this->_year = d._year;
-		this->_month = d._month;
-		this->_day = d._day;
+		_year = d._year;
+		_month = d._month;
+		_day = d._day;
 	}
 	return *this;
 }
 Date& Date::operator+=(int day) {
-	this->_day += day;
-	int mday = GetMonthDay(this->_year, this->_month);
-	while (this->_day > mday) {
-		if (this->_day > mday) {
-			this->_day -= mday;
-			this->_month++;
+	if (day >= 0) {
+		_day += day;
+		int mday = GetMonthDay(_year, _month);
+		while (_day > mday) {
+			if (_day > mday) {
+				_day -= mday;
+				_month++;
+			}
+			if (_month == 13) {
+				_month = 1;
+				_year++;
+			}
+			mday = GetMonthDay(_year, _month);
 		}
-		if (this->_month == 13) {
-			this->_month = 1;
-			this->_year++;
-		}
-		mday = GetMonthDay(this->_year, this->_month);
+	}
+	else {
+		*this -= (-day);
 	}
 	return *this;
 }
-Date Date::operator+(int day) {
+Date Date::operator+(int day) const {
 	Date tmp = *this;
-	tmp += day;
+	if (day >= 0) {
+		tmp += day;
+	}
+	else {
+		tmp -= (-day);
+	}
 	return tmp;
 }
 Date& Date::operator-=(int day) {
-	this->_day -= day;
-	int mday = GetMonthDay(this->_year, this->_month);
-	while (this->_day <= 0 ) {
-		if (this->_day <= 0) {
-			this->_month--;
-			mday = GetMonthDay(this->_year, this->_month);
-			this->_day += mday;
+	if (day >= 0) {
+		_day -= day;
+		int mday = GetMonthDay(_year, _month);
+		while (_day <= 0) {
+			if (_day <= 0) {
+				_month--;
+				mday = GetMonthDay(_year, _month);
+				_day += mday;
+			}
+			if (_month == 0) {
+				_month = 12;
+				_year--;
+			}
+			mday = GetMonthDay(_year, _month);
 		}
-		if (this->_month == 0) {
-			this->_month = 12;
-			this->_year--;
-		}		
-		mday = GetMonthDay(this->_year, this->_month);
+	}
+	else {
+		*this += (-day);
 	}
 	return *this;
 }
-Date Date::operator-(int day) {
+Date Date::operator-(int day) const {
 	Date tmp = *this;
-	tmp -= day;
+	if (day >= 0) {
+		tmp -= day;
+	}
+	else {
+		tmp += (-day);
+	}
 	return tmp;
 }
 Date& Date::operator++() {
@@ -101,75 +127,85 @@ Date Date::operator--(int) {
 	(*this) -= 1;
 	return tmp;
 }
-bool Date::operator>(const Date& d) {
-	if (this->_year > d._year) {
+bool Date::operator>(const Date& d) const {
+	if (_year > d._year) {
 		return true;
 	}
-	if (this->_year == d._year && this->_month > d._month) {
+	if (_year == d._year && _month > d._month) {
 		return true;
 	}
-	if (this->_year == d._year && this->_month == d._month && this->_day > d._day) {
-		return true;
-	}
-	return false;
-}
-bool Date::operator==(const Date& d) {
-	if (this->_year == d._year && this->_month == d._month && this->_day == d._day) {
+	if (_year == d._year && _month == d._month && _day > d._day) {
 		return true;
 	}
 	return false;
 }
-bool Date:: operator >= (const Date& d) {
+bool Date::operator==(const Date& d) const {
+	if (_year == d._year && _month == d._month && _day == d._day) {
+		return true;
+	}
+	return false;
+}
+bool Date:: operator >= (const Date& d) const {
 	return *this == d || *this > d;
 }
-bool  Date::operator < (const Date& d) {
+bool  Date::operator < (const Date& d) const {
 	return !(*this >= d);
 }
-bool  Date::operator <= (const Date& d) {
+bool  Date::operator <= (const Date& d) const {
 	return !(*this > d);
 }
-bool  Date::operator != (const Date& d) {
+bool  Date::operator != (const Date& d) const {
 	return !(*this == d);
 }
-int Date::operator-(const Date& d) {
-	int day = this->_day - d._day;
-	int lessmonth = this->_month < d._month ? this->_month : d._month;
-	int gratermonth= this->_month > d._month ? this->_month : d._month;
-	for (int i = lessmonth + 1; i <= gratermonth; i++) {
-		day += GetMonthDay(d._year, i);
+int Date::operator-(const Date& d) const {
+	Date max = *this;
+	Date min = d;
+	int count = 0, flag = 1;
+	if (*this < d) {
+		max = d;
+		min = *this;
+		flag *= -1;
 	}
-	int lessyear = this->_year < d._year ? this->_year : d._year;
-	int grateryear = this->_year > d._year ? this->_year : d._year;
-	for (int i = lessyear + 1; i <= grateryear; i++) {
-		day += GetYearDay(i);
+	while (min != max) {
+		++min;
+		count++;
 	}
-	return day;
+	return count * flag;
 }
-void Date::print() {
-	cout << _year << "-" << _month << "-" << _day << endl;
+ostream& operator<<(ostream& out, const Date& d) {
+	out << d._year << "年" << d._month << "月" << d._day << "日" <<endl;
+	return out;
 }
-
+istream& operator>>(istream& in, Date& d) {
+	int month, day;
+	in >> d._year >> month >> day;
+	if (month > 0 && month < 13 && day>0 && day <= d.GetMonthDay(d._year ,month)) {
+		d._month = month;
+		d._day = day;
+	}
+	else {
+		cout << "输入非法日期" << endl;
+		assert(false);
+	}
+	return in;
+}
 
 void date() {
-	Date d1;
-	d1.print();
+	Date d1(2023, 5, 5);
+	cout << d1;
 	d1 += 100;
-	d1.print();
-	d1-=11;
-	d1.print();
+	cout << d1;
 	Date d2 = ++d1;
-	d2.print();
-	d1.print();
+	cin >> d1;
+	cout << d1;
 	d2 = d1++;
-	d2.print();
-	d1.print();
-	cout << d2 - d1 << endl;
+	Date d3(2023, 5, 5);
+	Date d4(2000, 1, 1);
+	cout << d3 - d4 << endl;
+	cout << d4 - d3 << endl;
+
 	d2 = --d1;
-	d2.print();
-	d1.print();
 	d2 = d1--;
-	d2.print();
-	d1.print();
 }
 void test() {
 	Date d1;
@@ -178,6 +214,5 @@ void test() {
 int main() {
 	date();
 	//test();
-	Date d2;
 	return 0;
 }
