13141d6aa9cb6ad12e008374163a2e9a13db7ca2
mufengyaa <599348181@qq.com>
2023-06-10 21:39:51 +0800

    A simple program that simulates a shell

---
+++ myshell/myshell.c
@@ -0,0 +1,63 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<string.h>
+
+#define num 1024
+#define size 32
+#define SEP " "
+
+char cmd[num];
+char* options[size];
+
+int main(){
+  while(1){
+    printf("root@localhost myshell#");
+    fflush(stdout);
+    memset(cmd,'\0',sizeof cmd);
+    if(fgets(cmd,sizeof cmd,stdin)==NULL){
+      continue;
+    }//ctrl+z to quit
+    cmd[strlen(cmd)-1]='\0';
+    options[0]=strtok(cmd,SEP);
+    int i=1;
+      if(strcmp(options[0],"ls")==0){
+        options[i++]="--color=auto";
+      }
+    while(1){
+      options[i++]=strtok(NULL,SEP);
+      if(options[i-1]==NULL){
+        break;
+      }
+    }
+   // for(i=0;options[i];i++){
+   //   printf("%s\n",options[i]);
+   // }
+   
+    if(strcmp(options[0],"cd")==0){
+      if(options[1]!=NULL){
+        chdir(options[1]);  //change work_path
+      }
+      continue;
+    }
+    pid_t id=fork();
+    if(id==0){
+      printf("im child\n");
+      execvp(options[0],options);
+      exit(1);
+    }
+    else{
+      int status=0;
+      pid_t ret=waitpid(-1,&status,0);
+      if(ret>0){  //success
+        printf("%s:%d\n",options[0],WEXITSTATUS(status));
+      }
+      else{   //fail
+        printf("fail\n");
+      }
+    }
+  }
+  return 0;
+}
